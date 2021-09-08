ce0560c1c4e5a15a107adc5341e2bb753e110e6d
“[himanshu <[himanshudj3@gmail.com]>
2021-09-08 15:02:26 +0530

    azure deployment

--- src/main/java/com/javafsd/springazurejavafsdd/SpringAzureJavafsddApplication.java
+++ src/main/java/com/javafsd/springazurejavafsdd/SpringAzureJavafsddApplication.java
@@ -2,10 +2,13 @@ package com.javafsd.springazurejavafsdd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringAzureJavafsddApplication {
-
+@GetMapping("/msg")
 	public String message() {
 		return "Succesfully deployed on Azure";
 	}
